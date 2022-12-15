f4827fd8f135d48600606124f3040d7ae2989979
oleGAPrelev <101264107+oleGAPrelev@users.noreply.github.com>
2022-12-15 21:17:09 +0100

    youtube link added

--- r_f_invest_in_bulgarien/src/components/App/index.jsx
+++ r_f_invest_in_bulgarien/src/components/App/index.jsx
@@ -1,6 +1,7 @@
 import Header from '../Header';
 import Nav from '../Nav';
 import Services from '../Services';
+import YouTubeLink from '../YouTubeLink';
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Header />
 			<Nav />
 			<Services />
+			<YouTubeLink />
 		</div>
 	);
 }
---
+++ r_f_invest_in_bulgarien/src/components/YouTubeLink/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import s from './index.module.css';
+
+export default function YouTubeLink() {
+	return (
+		<div className={['wrapper', s.video_block].join(' ')}>
+			<div className={s.info_text}>
+				<h2>Immobilien in Bulgarien</h2>
+				<p>
+					Sehen Sie sich unser Video an und gehen Sie mit uns auf die Suche nach
+					Ihrem Traumhaus
+				</p>
+			</div>
+
+			<iframe
+				width="700"
+				height="515"
+				src="https://www.youtube.com/embed/ydUnPOZaDHU"
+				title="YouTube video player"
+				// frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				// allowfullscreen
+			></iframe>
+		</div>
+	);
+}
