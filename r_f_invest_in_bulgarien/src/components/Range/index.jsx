8904ade36e3393584941ef1de82730fc4816fb0d
Vitaliy Vynnyk <vitaliy.vinnik@gmail.com>
2023-02-01 10:27:38 +0100

    slider refactored

--- r_f_invest_in_bulgarien/src/components/Range/index.jsx
+++ r_f_invest_in_bulgarien/src/components/Range/index.jsx
@@ -3,7 +3,7 @@ import s from './index.module.css';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import {slick, apartments, room_2, room_3} from '../../data/slick';
+import {slick} from '../../data/slick';
 
 export default function Range() {
   const settings = {
@@ -11,7 +11,7 @@ export default function Range() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 600,
+    autoplaySpeed: 2500,
     pauseOnHover: false,
   };
   
@@ -19,18 +19,22 @@ export default function Range() {
     <div className={['wrapper', s.range_container].join(' ')}>
       <h1>Unsere Angebote</h1>
       <div className={s.block}>
-        {slick.map((wohnung) => {
+        {slick.map((wohnung, index) => {
           return (
-            <div className={s.slick}>
+            <div key={Date.now() + index} className={s.slick}>
               <Slider {...settings}>
                 {wohnung.images.map((image) => {
                   return (
-                    <div>
-                      <img src={image}/>
+                    <div key={image}>
+                      <img src={image} alt={wohnung.title + ' ' + image.slice(-5, -4).toUpperCase()}/>
                     </div>
                   )
                 })}
               </Slider>
+              <div className={s.price}>
+                <h2>{wohnung.title}</h2>
+                <p>{wohnung.price}</p>
+              </div>
             </div>
           )
         })}
--- r_f_invest_in_bulgarien/src/data/slick.js
+++ r_f_invest_in_bulgarien/src/data/slick.js
@@ -1,61 +1,38 @@
-export const apartments = [
-	'../../media/appartementen/appartementen_a.png',
-	'../../media/appartementen/appartementen_b.png',
-	'../../media/appartementen/appartementen_c.png',
-	'../../media/appartementen/appartementen_d.png',
-	'../../media/appartementen/appartementen_e.png',
-];
-
-export const room_2 = [
-	'../../media/zimmer_2/zimmer_2_a.png',
-	'../../media/zimmer_2/zimmer_2_b.png',
-	'../../media/zimmer_2/zimmer_2_c.png',
-	'../../media/zimmer_2/zimmer_2_d.png',
-	'../../media/zimmer_2/zimmer_2_e.png',
-];
-
-export const room_3 = [
-	'../../media/zimmer_3/zimmer_3_a.png',
-	'../../media/zimmer_3/zimmer_3_b.png',
-	'../../media/zimmer_3/zimmer_3_c.png',
-	'../../media/zimmer_3/zimmer_3_d.png',
-	'../../media/zimmer_3/zimmer_3_e.png',
-];
-
 export const slick = [
-	{
-		id: 1,
-		images: [
-				'../../media/appartementen/appartementen_a.png',
-				'../../media/appartementen/appartementen_b.png',
-				'../../media/appartementen/appartementen_c.png',
-				'../../media/appartementen/appartementen_d.png',
-				'../../media/appartementen/appartementen_e.png'
-		],
-		title: 'Appartament',
-		price: 'ab 35.000 Euro',
-	},
-	{
-		id: 2,
-		images: [	'../../media/zimmer_2/zimmer_2_a.png',
-			'../../media/zimmer_2/zimmer_2_b.png',
-			'../../media/zimmer_2/zimmer_2_c.png',
-			'../../media/zimmer_2/zimmer_2_d.png',
-			'../../media/zimmer_2/zimmer_2_e.png'
-		],
-		title: '2-Zimmer Wohnung',
-		price: 'ab 50.000 Euro',
-	},
-	{
-		id: 3,
-		images: [
-			'../../media/zimmer_3/zimmer_3_a.png',
-			'../../media/zimmer_3/zimmer_3_b.png',
-			'../../media/zimmer_3/zimmer_3_c.png',
-			'../../media/zimmer_3/zimmer_3_d.png',
-			'../../media/zimmer_3/zimmer_3_e.png'
-		],
-		title: '3-Zimmer Wohnung',
-		price: 'ab 70.000 Euro',
-	},
+  {
+    id: 1,
+    images: [
+      '../../media/appartementen/appartementen_a.png',
+      '../../media/appartementen/appartementen_b.png',
+      '../../media/appartementen/appartementen_c.png',
+      '../../media/appartementen/appartementen_d.png',
+      '../../media/appartementen/appartementen_e.png'
+    ],
+    title: 'Appartament',
+    price: 'ab 35.000 Euro',
+  },
+  {
+    id: 2,
+    images: [
+      '../../media/zimmer_2/zimmer_2_a.png',
+      '../../media/zimmer_2/zimmer_2_b.png',
+      '../../media/zimmer_2/zimmer_2_c.png',
+      '../../media/zimmer_2/zimmer_2_d.png',
+      '../../media/zimmer_2/zimmer_2_e.png'
+    ],
+    title: '2-Zimmer Wohnung',
+    price: 'ab 50.000 Euro',
+  },
+  {
+    id: 3,
+    images: [
+      '../../media/zimmer_3/zimmer_3_a.png',
+      '../../media/zimmer_3/zimmer_3_b.png',
+      '../../media/zimmer_3/zimmer_3_c.png',
+      '../../media/zimmer_3/zimmer_3_d.png',
+      '../../media/zimmer_3/zimmer_3_e.png'
+    ],
+    title: '3-Zimmer Wohnung',
+    price: 'ab 70.000 Euro',
+  },
 ];
