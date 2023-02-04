dc996063c6842afdc12ac0ab8a69f875aa0f9558
oleGAPrelev <101264107+oleGAPrelev@users.noreply.github.com>
2023-02-04 15:59:04 +0100

    consultation background video  added

--- src/components/Consultation/index.jsx
+++ src/components/Consultation/index.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import ConsultationForm from '../ConsultationForm';
 import s from './index.module.css';
+import video from './Video/video_bg.mp4';
 
 export default function Consultation() {
-	
-	
 	return (
-		<section className={s.beratung_container} id="consultant">
-			<div className={['wrapper', s.beratung_block].join(' ')}>
-				<div>
-					<h2>Brauchen Sie eine Beratung?</h2>
-					<p>Kontaktieren Sie uns und wir helfen Ihnen</p>
-				</div>
+		<section className={s.consultation_container} id="consultant">
+			<video src={video} autoplay="start" muted loop></video>
+			<div className={s.effect}>
+				<div className={['wrapper', s.consultation_block].join(' ')}>
+					<div className={s.consultation_text}>
+						<h2>Brauchen Sie eine Beratung?</h2>
+						<p>Kontaktieren Sie uns und wir helfen Ihnen</p>
+					</div>
 
-				<div>
-					<ConsultationForm />
+					<div>
+						<ConsultationForm />
+					</div>
 				</div>
 			</div>
 		</section>
--- src/components/ConsultationForm/index.jsx
+++ src/components/ConsultationForm/index.jsx
@@ -1,51 +1,24 @@
 import React from 'react';
-import {useForm} from 'react-hook-form';
 import Button from '../../UI/Button';
 import Input from '../../UI/Input';
 import s from './index.module.css';
+
 export default function ConsultationForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: {errors},
-    reset
-  } = useForm({mode: 'onBlur'});
-  
-  const formHandleSubmit = (data) => {
-    fetch('http://localhost:3002/send', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-      .then((response) => (response.json()))
-      .then((response) => {
-        if (response.status === 'success') {
-          alert('Message Sent.');
-        } else if (response.status === 'fail') {
-          alert('Message failed to send.')
-        }
-      });
-    reset();
-  }
-  
-  return (
-    <form className={s.consultation_form} onSubmit={handleSubmit(formHandleSubmit)}>
-      <div className={s.daten_block}>
-        <label>
-          <p>Vorname, Nachname</p>
-          <Input {...register('name', {required: true})}/>
-          {errors.name && <span className={s.error}>Dieses Feld ist erforderlich</span>}
-        </label>
-        
-        <label>
-          <p>Telefonnummer</p>
-          <Input {...register('phone', {required: true})}/>
-          {errors.phone && <span className={s.error}>Dieses Feld ist erforderlich</span>}
-        </label>
-      </div>
-      <Button type={'submit'}>Kontaktiere Uns</Button>
-    </form>);
+	return (
+		<form className={s.consultation_form}>
+			<div className={s.daten_block}>
+				<label>
+					<p>Vorname, Nachname</p>
+					<Input />
+				</label>
+
+				<label>
+					<p>Telefonnummer</p>
+					<Input />
+				</label>
+			</div>
+
+			<Button>Kontaktiere Uns</Button>
+		</form>
+	);
 }
