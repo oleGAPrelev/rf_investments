d4239db1783df401bb08b456569e30d359839531
yulia-bisyuk <yulia.bisyuk@gmail.com>
2023-02-14 13:34:15 +0200

    feat: footer styles finished

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,4 @@
 import React from 'react';
-// import Logo from '../../UI/Logo';
-// import Address from './FooterComponents/Address';
-// import Map from './FooterComponents/Map';
-// import Socials from '../../UI/Socials';
-// import PhoneContacts from './FooterComponents/PhoneContacts';
-// import FooterNav from './FooterComponents/FooterNav';
 import useMediaQuery from '../../hooks/useMediaQuery';
 import MobileTabletView from './MobileTabletView';
 import DesktopView from './DesktopView';
@@ -20,22 +14,6 @@ export default function Footer({ setModal }) {
       ) : (
         <MobileTabletView setModal={setModal} />
       )}
-
-      {/* <div className={s.footer_logo_address_wrapper}>
-          <Logo />
-          <Address />
-        </div>
-
-        <div className={s.footer_map_wrapper}>
-          <Map />
-        </div>
-
-        <div className={s.footer_socials_phones_wrapper}>
-          <Socials placement='footer' />
-          <PhoneContacts />
-        </div>
-
-        <FooterNav setModal={setModal} /> */}
     </footer>
   );
 }
