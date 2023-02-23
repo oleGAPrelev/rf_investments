84097f8a68357830896670c9cc466a777eb04551
yulia-bisyuk <yulia.bisyuk@gmail.com>
2023-02-23 20:33:48 +0200

    feat: adds responsive layout to all sections except Consultations

--- src/components/Consultation/index.jsx
+++ src/components/Consultation/index.jsx
@@ -4,21 +4,26 @@ import s from './index.module.css';
 import video from './Video/video_bg.mp4';
 
 export default function Consultation() {
-	return (
-		<section className={s.consultation_container} id="consultant">
-			<video src={video} autoPlay="start" muted loop></video>
-			<div className={s.effect}>
-				<div className={['wrapper', s.consultation_block].join(' ')}>
-					<div className={s.consultation_text}>
-						<h2>Brauchen Sie eine Beratung?</h2>
-						<p>Kontaktieren Sie uns und wir helfen Ihnen</p>
-					</div>
+  return (
+    <section
+      className={['section', s.consultation_section].join(' ')}
+      id='consultant'
+    >
+      <div className={['container', s.consultation_container].join(' ')}>
+        <video src={video} autoPlay='start' muted loop></video>
+        <div className={s.effect}>
+          <div className={['wrapper', s.consultation_block].join(' ')}>
+            <div className={s.consultation_text}>
+              <h2>Brauchen Sie eine Beratung?</h2>
+              <p>Kontaktieren Sie uns und wir helfen Ihnen</p>
+            </div>
 
-					<div>
-						<ConsultationForm />
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+            <div>
+              <ConsultationForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/CustomerReviews/index.jsx
+++ src/components/CustomerReviews/index.jsx
@@ -5,15 +5,17 @@ import runge from './media/runge.png';
 import hofman from './media/hofman.png';
 
 export default function CustomerReviews() {
-	return (
-		<div className={['wrapper', s.customer_container].join(' ')}>
-			<h1>Was unsere Kunden sagen über uns</h1>
+  return (
+    <section className='section'>
+      <div className={'container'}>
+        <h2 className={s.reviews_title}>Was unsere Kunden sagen über uns</h2>
 
-			<div className={s.images}>
-				<img src={seher} alt="seher" />
-				<img src={runge} alt="runge" />
-				<img src={hofman} alt="hofman" />
-			</div>
-		</div>
-	);
+        <div className={s.reviews_images}>
+          <img src={seher} alt='seher' />
+          <img src={runge} alt='runge' />
+          <img src={hofman} alt='hofman' />
+        </div>
+      </div>
+    </section>
+  );
 }
