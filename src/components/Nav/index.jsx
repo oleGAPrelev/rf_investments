9afc6edd48ad3092ccd51ddfb9fb8b33ecfb0912
Vitaliy Vynnyk <Vitaliy.vinnik@gmail.com>
2023-03-30 19:16:20 +0200

    started working on fixes

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,44 +1,46 @@
 import React from 'react';
-import { Link } from 'react-scroll';
+import {Link} from 'react-scroll';
 import Button from '../../UI/Button';
 import s from './index.module.css';
 import Logo from '../../UI/Logo';
 
 export default function Nav() {
-	return (
-		<section className={s.nav}>
-			<div className="container">
-				<div className={s.nav_block}>
-					<Logo />
-					<ul className={s.nav_menu}>
-						<Link
-							to="consultant"
-							spy={true}
-							smooth={true}
-							offset={50}
-							duration={500}
-						>
-							Kontakt
-						</Link>
-					</ul>
-				</div>
-				<div className={s.nav_contacts}>
-					<h1>Träumen Sie von einer Immobilie am Meer?</h1>
-					<p>
-						Wenden Sie sich an uns und unsere Qualifizierten Mitarbeiter werden
-						eine Lösung für Sie finden.
-					</p>
-					<Link
-						to="consultant"
-						spy={true}
-						smooth={true}
-						offset={50}
-						duration={500}
-					>
-						<Button>Kontaktiere uns</Button>
-					</Link>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className={s.nav}>
+      <div className="container">
+        <div className={s.nav_block}>
+          <Logo/>
+          {/*<ul className={s.nav_menu}>*/}
+          {/*	<Link*/}
+          {/*		to="consultant"*/}
+          {/*		spy={true}*/}
+          {/*		smooth={true}*/}
+          {/*		offset={50}*/}
+          {/*		duration={500}*/}
+          {/*	>*/}
+          {/*		Kontakt*/}
+          {/*	</Link>*/}
+          {/*</ul>*/}
+        </div>
+        <div className={s.nav_contacts}>
+          <h1>Träumen Sie von einer Immobilie am Meer?</h1>
+          <p className={s.header_description}>
+            Wenden Sie sich an uns und unsere Qualifizierten Mitarbeiter werden
+            eine Lösung für Sie finden.
+          </p>
+          <div className={s.contact_button_wrapper}>
+            <Link
+              to="consultant"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              <Button>Kontaktiere uns</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
