d0642934f5f89c273327b648f836e72febe4a30e
Vitaliy Vynnyk <Vitaliy.vinnik@gmail.com>
2023-04-01 10:10:14 +0200

    bug to see

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -6,38 +6,28 @@ import Logo from '../../UI/Logo';
 
 export default function Nav() {
   return (
-    <section className={s.nav}>
-      <div className="container">
-        <div className={s.nav_block}>
-          <Logo/>
-          {/*<ul className={s.nav_menu}>*/}
-          {/*	<Link*/}
-          {/*		to="consultant"*/}
-          {/*		spy={true}*/}
-          {/*		smooth={true}*/}
-          {/*		offset={50}*/}
-          {/*		duration={500}*/}
-          {/*	>*/}
-          {/*		Kontakt*/}
-          {/*	</Link>*/}
-          {/*</ul>*/}
-        </div>
-        <div className={s.nav_contacts}>
-          <h1>Träumen Sie von einer Immobilie am Meer?</h1>
-          <p className={s.header_description}>
-            Wenden Sie sich an uns und unsere Qualifizierten Mitarbeiter werden
-            eine Lösung für Sie finden.
-          </p>
-          <div className={s.contact_button_wrapper}>
-            <Link
-              to="consultant"
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-            >
-              <Button>Kontaktiere uns</Button>
-            </Link>
+    <section className={s.nav} style={{backgroundColor: 'yellow'}}>
+      <div id={'test'} style={{backgroundColor: 'red', minHeight: '100%'}}>
+        <div className={'container'} style={{backgroundColor: 'green', height: '100%'}}>
+          <div className={[s.nav_contacts, s.headlines].join(' ')}>
+            <Logo/>
+            <h1>Träumen Sie von einer Immobilie am Meer?</h1>
+            <p className={s.header_description}>
+              Wenden Sie sich an uns und unsere Qualifizierten Mitarbeiter werden
+              eine Lösung für Sie finden.
+            </p>
+            <div className={s.contact_button_wrapper}>
+              <Link
+                to="consultant"
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+              >
+                <Button>Kontaktiere uns</Button>
+              </Link>
+            </div>
+          
           </div>
         </div>
       </div>
