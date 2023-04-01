b682e2c7cb24f68f0f93f5b800a2291232ea9d67
yulia-bisyuk <yulia.bisyuk@gmail.com>
2023-04-01 16:43:13 +0300

    icons changed

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,57 +1,61 @@
 import React from 'react';
 import s from './index.module.css';
-import { IoStatsChartOutline } from 'react-icons/io5';
-import { SlTag } from 'react-icons/sl';
-import { BsPercent } from 'react-icons/bs';
-import { HiOutlineScale } from 'react-icons/hi';
-import { SlHome } from 'react-icons/sl';
+import { ReactComponent as GraphicsScaleIcon } from './media/graphics-scale.svg';
+import { ReactComponent as MoneyPriceIcon } from './media/money_price_icon.svg';
+import { ReactComponent as PercentIcon } from './media/discount_percent_icon.svg';
+import { ReactComponent as LawIcon } from './media/law_icon.svg';
+import { ReactComponent as HouseIcon } from './media/house_icon.svg';
 
 export default function Services() {
-	return (
-		<section className="section">
-			<div className={['container', s.services_container].join(' ')}>
-				<h2>
-					<span className={s.accent}>R & F Investments</span> in&nbsp;Bulgarien
-				</h2>
+  return (
+    <section className='section'>
+      <div className={['container', s.services_container].join(' ')}>
+        <h2>
+          <span className={s.accent}>R & F Investments</span> in&nbsp;Bulgarien
+        </h2>
 
-				<div className={s.services_content_container}>
-					<div className={s.services_item_container}>
-						<IoStatsChartOutline className={s.services_icon} />
-						<p>
-							Unser Unternehmen arbeitet nur mit marktführenden Bauträgern in
-							Bulgarien
-						</p>
-					</div>
+        <div className={s.services_content_container}>
+          <div className={s.services_item_container}>
+            <GraphicsScaleIcon className={s.services_icon} />
+            <p>
+              Unser Unternehmen arbeitet nur mit marktführenden Bauträgern in
+              Bulgarien
+            </p>
+          </div>
 
-					<div className={s.services_item_container}>
-						<SlTag className={s.services_icon} />
-						<p>
-							Günstiger Preis direkt vom Bauträger ohne Aufpreis und Provisionen
-						</p>
-					</div>
+          <div className={s.services_item_container}>
+            <MoneyPriceIcon className={s.services_icon} />
+            <p>
+              Günstiger Preis direkt vom Bauträger ohne Aufpreis und Provisionen
+            </p>
+          </div>
 
-					<div className={s.services_item_container}>
-						<BsPercent className={s.services_icon} />
-						<p>Zinsfreier Teilzahlungsplan für den Erwerb von Wohnräumen</p>
-					</div>
+          <div className={s.services_item_container}>
+            <PercentIcon className={s.services_icon} />
+            <p>
+              Zinsfreier Teilzahlungsplan für den Erwerb von 
+              <br className={s.services_break} />
+              Wohnräumen
+            </p>
+          </div>
 
-					<div className={s.services_item_container}>
-						<HiOutlineScale className={s.services_icon} />
-						<p>
-							Kostenlose Rechtsberatung durch unsere Vertragsanwälte bei der
-							Transaktionsabwicklung
-						</p>
-					</div>
+          <div className={s.services_item_container}>
+            <LawIcon className={s.services_icon} />
+            <p>
+              Kostenlose Rechtsberatung durch unsere Vertragsanwälte bei der
+              Transaktionsabwicklung
+            </p>
+          </div>
 
-					<div className={s.services_item_container}>
-						<SlHome className={s.services_icon} />
-						<p>
-							Unterstützung bei der Vermietung der Immobilien. Technische und
-							Kaufmännische Verwaltung
-						</p>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+          <div className={s.services_item_container}>
+            <HouseIcon className={s.services_icon} />
+            <p>
+              Unterstützung bei der Vermietung der Immobilien. Technische und
+              Kaufmännische Verwaltung
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
